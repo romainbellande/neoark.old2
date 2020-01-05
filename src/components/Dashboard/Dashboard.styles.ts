d728720a97ac3ed85e2db6a04fd4155a48f741e5
Naimor <romain.bellande@orange.fr>
2020-01-05 22:03:13 +0100

    style: improve main background (#29)
    close: #28

--- src/components/Dashboard/Dashboard.styles.ts
+++ src/components/Dashboard/Dashboard.styles.ts
@@ -2,7 +2,34 @@ import { Theme, makeStyles } from '@material-ui/core';
 
 export const drawerWidth = 256;
 
+const getRandomInt = (max: number) => {
+  return Math.floor(Math.random() * Math.floor(max));
+};
+
+const multipleBoxShadow = (n: number) => {
+  let value = `${getRandomInt(2000)}px ${getRandomInt(2000)}px #FFF`;
+
+  for (let i = 2; i < n; i += 1) {
+    value = `${value} , ${getRandomInt(2000)}px ${getRandomInt(2000)}px #FFF`;
+  }
+
+  return value;
+};
+
+const shadowsSmall = multipleBoxShadow(700);
+const shadowsMedium = multipleBoxShadow(200);
+const shadowsBig = multipleBoxShadow(100);
+const starSpeedBase = 100; // higher value will slow the animation
+
 export default makeStyles((theme: Theme) => ({
+  '@keyframes animStar': {
+    from: {
+      transform: 'translateY(0px)',
+    },
+    to: {
+      transform: 'translateY(-2000px)',
+    },
+  },
   root: {
     display: 'flex',
     minHeight: '100vh',
@@ -25,7 +52,7 @@ export default makeStyles((theme: Theme) => ({
     position: 'relative',
   },
   mainBackgroundImage: {
-    backgroundColor: '#5d748c',
+    background: 'radial-gradient(ellipse at bottom, #1B2735 0%, #090A0F 100%)',
     position: 'absolute',
     width: '100%',
     height: '100%',
@@ -41,5 +68,57 @@ export default makeStyles((theme: Theme) => ({
   },
   infoNav: {
     height: '100%',
+  },
+  stars1: {
+    width: '1px',
+    height: '1px',
+    background: 'transparent',
+    boxShadow: shadowsSmall,
+    animation: `$animStar ${starSpeedBase}s linear infinite`,
+    '&:after': {
+      content: ' ',
+      position: 'absolute',
+      top: '2000px',
+      width: '1px',
+      height: '1px',
+      background: 'transparent',
+      boxShadow: shadowsSmall,
+    },
+  },
+  stars2: {
+    width: '2px',
+    height: '2px',
+    background: 'transparent',
+    boxShadow: shadowsMedium,
+    animation: `$animStar ${starSpeedBase * 2}s linear infinite`,
+    '&:after': {
+      content: ' ',
+      position: 'absolute',
+      top: '2000px',
+      width: '2px',
+      height: '2px',
+      background: 'transparent',
+      boxShadow: shadowsMedium,
+    },
+  },
+  stars3: {
+    width: '3px',
+    height: '3px',
+    background: 'transparent',
+    boxShadow: shadowsBig,
+    animation: `$animStar ${starSpeedBase * 3}s linear infinite`,
+    '&:after': {
+      content: ' ',
+      position: 'absolute',
+      top: '2000px',
+      width: '3px',
+      height: '3px',
+      background: 'transparent',
+      boxShadow: shadowsBig,
+    },
+  },
+  mainRoute: {
+    zIndex: 100,
+    position: 'relative',
   },
 }));
